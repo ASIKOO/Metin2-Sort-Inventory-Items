38a2b6f74f3ce264cbb113bd1f1fc4a9b9130004
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2018-04-23 22:29:45 +0300

    Update char_item.cpp

--- 1.Svn/Server/game/src/char_item.cpp
+++ 1.Svn/Server/game/src/char_item.cpp
@@ -5,3 +5,8 @@ static bool FN_check_item_socket(LPITEM item)
 ///Add(Coppied from cTolip)
 	if (item->GetType() == ITEM_USE && item->GetSubType() == USE_AFFECT)
 		return true;
+
+//Find in LPITEM CHARACTER::AutoGiveItem(DWORD dwItemVnum, BYTE bCount, int iRarePct, bool bMsg)
+	if (item->GetVnum() == dwItemVnum && FN_check_item_socket(item))
+///Change
+	if (item->GetOriginalVnum() == dwItemVnum && FN_check_item_socket(item))
