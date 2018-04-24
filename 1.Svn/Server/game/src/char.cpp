65c78bcf6e021ad7a07b7bcfe55c8df18188e005
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2018-04-24 18:02:17 +0300

    Update char.cpp

--- 1.Svn/Server/game/src/char.cpp
+++ 1.Svn/Server/game/src/char.cpp
@@ -45,7 +45,7 @@ void CHARACTER::EditMyInven()
 			TItemTable * p = ITEM_MANAGER::instance().GetTable(item->GetVnum());
 			/// isn't same function !
 			if (p && p->dwFlags & ITEM_FLAG_STACKABLE && p->bType != ITEM_BLEND)
-				AutoGiveItem(item->GetVnum(), item->GetCount(), -1, false); // create new item for stackable items + msg bug fixed
+				AutoGiveItem(item->GetVnum(), item->GetCount(), -1, false); // create new item for stackable items
 			else
 				AutoGiveItem(item); // copy orginal items
 		}
