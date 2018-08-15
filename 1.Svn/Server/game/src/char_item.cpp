8aba9505c16e1e210089348d3f1cecbf60074ddf
Muhammet Ali **** <33325253+blackdragonx61@users.noreply.github.com>
2018-08-15 10:23:32 +0300

    Update char_item.cpp

--- 1.Svn/Server/game/src/char_item.cpp
+++ 1.Svn/Server/game/src/char_item.cpp
@@ -13,7 +13,7 @@ static bool FN_check_item_socket(LPITEM item)
 //Find in LPITEM CHARACTER::AutoGiveItem(DWORD dwItemVnum, BYTE bCount, int iRarePct, bool bMsg)
 	if (item->GetVnum() == dwItemVnum && FN_check_item_socket(item))
 ///Change
-#ifdef #ifdef ENABLE_SORT_INVEN
+#ifdef ENABLE_SORT_INVEN
 	if (item->GetOriginalVnum() == dwItemVnum && FN_check_item_socket(item))
 #else
 	if (item->GetVnum() == dwItemVnum && FN_check_item_socket(item))	
